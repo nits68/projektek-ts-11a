a6fe065f5e0ba2e7766e81c82e0b851acfa0dd6f
nits68 <nits.laszlo@jedlik.eu>
2025-09-12 12:29:18 +0200

    óra vége

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
 import Image from 'next/image';
+import Link from 'next/link';
+
+type TLink = {
+  id: number;
+  href: string;
+  label: string;
+};
+
+const links: TLink[] = [
+  { id: 1, href: '/counter', label: 'Számláló' },
+];
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-200">
       <div className="flex flex-col items-center justify-center rounded-lg bg-white p-4 shadow-md">
-        <Image src="globe.svg" alt="Globe" width={100} height={100} />
-        <h1 className="text-2xl font-bold mt-2">Hello World!</h1>
+        <Image src="globe.svg" alt="Globe" width="100" height={100} />
+        <h1 className="mt-2 text-2xl font-bold">Hello World!</h1>
+        <ul className="mt-3 list-disc">
+          {links.map((link) => (
+            <li key={link.id}>
+              <Link href={link.href} className="text-blue-500 hover:underline" >
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
